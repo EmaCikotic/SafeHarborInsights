bebc3497bdc6036c3113a188fa846bcf5005c55d
ema.cikotic <emacikotic25@gmail.com>
2024-08-03 22:49:58 +0200

    Changed the dataset to a reduced version where only needed attributes are used and implement code for reading the data and part of the function which will be analysing the data

--- src/index.ts
+++ src/index.ts
@@ -20,3 +20,54 @@ fs.createReadStream(datasetPath)
   .on("end", () => {
     console.log("CSV file successfully processed.");
   });
+
+function dataAnalysis() {
+  //needed feelds converted to numberical format for clustering
+  const crimeByArea: { [area: string]: number } = {};
+  const crimeByAreaAndGender: {
+    [area: string]: { male: number; female: number };
+  } = {};
+
+  crimeData.forEach((record) => {
+    const area = record.area;
+    const gender = record.gender.toLowerCase();
+
+    //counting crimes by area
+    if (!crimeByArea[area]) {
+      crimeByArea[area] = 0;
+    }
+    crimeByArea[area]++;
+
+    //counting by area and gender
+    //counting crimes by area
+    if (!crimeByAreaAndGender[area]) {
+      crimeByAreaAndGender[area] = { male: 0, female: 0 };
+    }
+    if (gender === "male" || gender === "female") {
+      crimeByAreaAndGender[area][gender]++;
+    }
+  });
+
+  ///converting counts to arrays for clustering
+  const areaNames = Object.keys(crimeByArea);
+  const crimeCounts = areaNames.map((area) => [crimeByArea[area]]);
+  const crimeCountsGender = areaNames.map((area) => [
+    crimeByAreaAndGender[area].male,
+    crimeByAreaAndGender[area].female,
+  ]);
+
+  //calling the k means clustering
+  //set the number of clusters
+  //call the functions for  general resukts and for gender results
+}
+
+//TODO
+//k clustering algorithm
+
+//initialization of centroids
+
+//assigning clusters
+
+//updating centroids
+
+//euclidian distance
