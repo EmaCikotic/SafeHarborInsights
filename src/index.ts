d10619776034c07b289b47f22e462a9311d469ec
ema.cikotic <emacikotic25@gmail.com>
2024-08-03 18:37:19 +0200

    Reading csv file

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,22 @@
 import * as fs from "fs";
-import * as csv from "csv-parser";
+import csv from "csv-parser";
 
+interface CrimeRecord {
+  area: string;
+  gender: string;
+}
+
+//saving a dataset in variabe so it can be used  later
+const datasetPath = "./data/Crime_Data_from_2020_to_Present.csv";
+
+const crimeData: CrimeRecord[] = [];
+
+// Read the CSV file
+fs.createReadStream(datasetPath)
+  .pipe(csv())
+  .on("data", (row: CrimeRecord) => {
+    crimeData.push(row);
+  })
+  .on("end", () => {
+    console.log("CSV file successfully processed.");
+  });
